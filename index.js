bf2ceffb253c5b2ef8302d6135b2ccb080a3545b
Ella <michaella.latkovic@gmail.com>
2019-10-04 12:10:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+ function saturdayFun(argument = "roller-skate") {
+  return (`This Saturday, I want to ${argument}!`);
+ }
+
+ let mondayWork = function (argument = "go to the office") {
+  return (`This Monday, I will ${argument}.`);
+ }
+
+function wrapAdjective(flare = "*"){
+  return function secondFunction(argument = "special"){
+    return (`You are ${flare}${argument}${flare}!`);
+  }
+}
+
+let Calculator = {
+  add: function(a,b){
+    return a + b;
+  },
+  subtract: function(a,b){
+    return a - b;
+  },
+  multiply: function(a,b){
+    return a * b;
+  },
+  divide: function(a,b){
+    return a / b;
+  }
+}
+
+function actionApplyer(integer, array){
+  for (let i = 0; i < array.length; i++ ){
+    integer = array[i](integer);
+  }
+return  integer;
+}
