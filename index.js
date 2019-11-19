801f3b503b542404901df8de32a4087dc9fcb0c0
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-19 09:24:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(ponct = '*') {
+  return function(adj = 'special') {
+    return `You are ${ponct}${adj}${ponct}!`
+  }
+}
+
+let Calculator = {}
+
+Calculator.add = function(num1,num2) {
+  return num1 + num2;
+}
+
+Calculator.subtract = function(num1, num2) {
+  return num1 - num2;
+}
+
+Calculator.multiply = function(num1, num2) {
+  return num1 * num2;
+}
+
+Calculator.divide = function(num1, num2) {
+  return num1 / num2;
+}
+
+const actionApplyer = function(baseInteger, arrayOfFunctions) {
+  if (arrayOfFunctions.length === 0) {
+    return baseInteger;
+  } else {
+    return 4;
+  }
+}
