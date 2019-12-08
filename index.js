bf62b4e446c4a35c6f5711bdd9927abe9737819c
Anthony Graniero <ananfro@gmail.com>
2019-12-08 21:23:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+function saturdayFun(activity='roller-skate'){
+  return `This Saturday, I want to ${activity}!`;
+}
+
+var mondayWork = function(activity='go to the office'){
+  return `This Monday, I will ${activity}.`;
+};
+
+var wrapAdjective = function(boopity='*'){
+  return function(boop='special'){
+    return `You are ${boopity}${boop}${boopity}!`;
+  };
+};
+
+var Calculator={
+  add: (a,b) => a+b,
+  subtract: (a,b) => a-b,
+  multiply: (a,b) => a*b,
+  divide: (a,b) => a/b
+};
+
+var actionApplyer = function(start, ray){
+  let boo = start;
+  for (let i=0; i<ray.length; i++){
+    boo=ray[i](boo);
+  }
+  return boo;
+};
+  
