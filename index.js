905a78ad14d06d711889b95a3f4b4e38394cf80a
Kristinamarih <kristinahorensky@gmail.com>
2019-12-09 12:19:47 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity='go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+
+
