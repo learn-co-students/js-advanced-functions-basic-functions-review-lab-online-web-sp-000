4feb4f28935efcfc92f2b1e6fbb8c91995105340
boom100100 <bsd231@nyu.edu>
2020-01-21 20:02:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+
+function saturdayFun(doSomething){
+  return `This Saturday, I want to ${doSomething}!`
+}
+
+function mondayWork(doSomething = "go to the office"){
+  return `This Monday, I will ${doSomething}.`
+}
