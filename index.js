419c17901ed34007da7190c28cd65639c04ccf92
Bernadette Davis <bsd231@nyu.edu>
2020-01-25 20:34:03 +0000

    Done.

--- index.js
+++ index.js
@@ -20,15 +20,25 @@ function mondayWork(doSomething = "go to the office"){
 
 function wrapAdjective(symbol){
   
-  if (symbol === '*'){
-    return (function(adj){return `You are *${adj}*!`;})
-  } else if (symbol === '||') {
-    return (function(adj){return `You are ||${adj}||`;})
+  
+  
+  if (symbol === '||' || symbol === '*') {
+    return (function(adj){return `You are ${symbol}${adj}${symbol}!`;});
   }
   
 }
 
 
 function actionApplyer(startInt, arrayFcns){
+  if (arrayFcns.length === 0){
+    return startInt;
+  } else {
+    
+    for (let i = 0; i < arrayFcns.length; i++){
+      console.log(arrayFcns[i].toString());
+      startInt = arrayFcns[i](startInt);
+    }
+    return startInt;
+  }
   
 }
