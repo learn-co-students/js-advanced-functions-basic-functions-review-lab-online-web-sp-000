41e4be88bdb184af1b1c2c2ca6dcc6a4c7ed0af4
Bernadette Davis <bsd231@nyu.edu>
2020-01-25 20:03:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 // Your code here
 
-function saturdayFun(doSomething){
+
+let Calculator = {
+  add: function(a, b){ return a+b;},
+  subtract: function(a, b){ return a-b;},
+  multiply: function(a, b){ return a*b;},
+  divide: function(a, b){ return a/b;}
+}
+  
+
+
+function saturdayFun(doSomething = 'roller-skate'){
   return `This Saturday, I want to ${doSomething}!`
 }
 
 function mondayWork(doSomething = "go to the office"){
   return `This Monday, I will ${doSomething}.`
+}
+
+function wrapAdjective(string){
+  
+}
+
+function actionApplyer(startInt, arrayFcns){
+  
 }
