4c4eeb857874631ab77085df4e92c1f1cf281253
wyao905 <warrenyca905@gmail.com>
2020-01-27 21:43:30 -0500

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`
 }
 
-mondayWork = function(work = "go to the office") {
+let mondayWork = function(work = "go to the office") {
     return `This Monday, I will ${work}.`
 }
 
