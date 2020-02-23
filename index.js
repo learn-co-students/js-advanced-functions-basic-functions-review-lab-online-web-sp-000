55e03bacf0d283b4b9dc1ba0736aed82811e0022
tomciv9000 <eugene.white@gmail.com>
2020-02-23 14:56:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+//   Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair='*'){
+    return function(innerParam='special'){
+        return `You are ${flair}${innerParam}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: function(a,b){
+        return a + b;
+    },
+    subtract: function(a,b){
+        return a - b;
+    },
+    multiply: function(a,b){
+        return a * b;
+    },
+    divide: function(a,b){
+        return a /b;
+    }
+}
+
+function actionApplyer(first, theArray){
+    let a = first;
+
+    for (let i=0; i < theArray.length; i++){
+        a = theArray[i](a)
+    }
+
+    return a;
+}
