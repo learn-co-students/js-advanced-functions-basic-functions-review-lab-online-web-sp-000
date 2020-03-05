0804c6f831592f035c3ee257f9c01a1ad6ab77dc
Freddy Silber <freddy.silber@cloudcoach.com>
2020-03-05 18:22:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+const saturdayFun = (activity = 'roller-skate') => {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (task = 'go to the office') => {
+  return `This Monday, I will ${task}.`
+}
+
+const wrapAdjective = (style = '*') => {
+  return (adjective = 'special') => {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
+const Calculator = {
+  add: (a, b) => {
+    return a + b
+  },
+  subtract: (a, b) => {
+    return a - b
+  },
+  multiply: (a, b) => {
+    return a * b
+  },
+  divide: (a, b) => {
+    return a / b
+  }
+}
+
+
+const actionApplyer = (start, arr) => {
+  let a = start
+  for (let i = 0; i < arr.length; i++) {
+    a = arr[i](a)
+  }
+  return a
+}
