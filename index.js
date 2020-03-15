f5fc5d587b1dcc1320e30f2e754d26fc7689c48e
github username <github email address>
2020-03-15 15:57:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+function saturdayFun(activity='roller-skate'){
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work='go to the office'){
+  return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair='*'){
+  return function(adj='special'){
+    return `You are ${flair}${adj}${flair}!`
+  }
+}
+
+let Calculator = {
+  add: function(num1, num2){ return num1 + num2 },
+  subtract: function(num1, num2){ return num1 - num2 },
+  multiply: function(num1, num2){ return num1 * num2 },
+  divide: function(num1, num2){ return num1 / num2 }
+}
+
+function actionApplyer(int, funcs){
+  let finalVal = int
+  funcs.forEach(func => {
+    finalVal = func(finalVal)
+  })
+  return finalVal
+}
