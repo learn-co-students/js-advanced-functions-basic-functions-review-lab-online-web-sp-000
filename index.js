ab0d2df1172b6db1c81636c321eda85df5189dfe
Ronald Stephens <rstephens@bluemorpho.cc>
2020-04-01 15:59:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+// saturdayFun()
+    function saturdayFun(stuffToDo= 'roller-skate') {
+        console.log(`This Saturday, I want to ${stuffToDo}!`);
+        return (`This Saturday, I want to ${stuffToDo}!`);
+    }
+
+    saturdayFun();
+
+// mondayWork()
+    function mondayWork(stuffToDo = 'go to the office') {
+        console.log(`This Monday, I will ${stuffToDo}.`);
+        return (`This Monday, I will ${stuffToDo}.`);
+    }
+
+    mondayWork();
+
+// wrapAdjective()
+    function wrapAdjective(flair= "*") {
+        let innerFunction = function (variable = "special") {
+            return (`You are ${flair}${variable}${flair}!`)
+         }
+        return innerFunction
+    }
+
+    wrapAdjective();
+
+// Calculator
+    const Calculator = new Object();
+    Calculator.add = function() {return 1 + 3;}
+    Calculator.subtract = function() {return 1 - 3;}
+    Calculator.multiply = function() {return 1 * 3;}
+    Calculator.divide = function() { return 10 / 5;}
+
+// actionApplyer
+    function actionApplyer(startingInteger, array) {
+        if (array === undefined || array.length === 0) {
+            return startingInteger;
+        } else {
+           return array.reduce((previousResult, fn) => fn(previousResult), startingInteger);
+        }
+    }
