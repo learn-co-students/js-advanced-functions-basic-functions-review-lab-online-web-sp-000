433d1a9e0457a81146bcb29e7a29032237db43aa
RobinSanta <robinsntn@gmail.com>
2020-04-24 23:11:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 function saturdayFun(activity="roller-skate") {
-    console.log(`This Saturday, I want to ${activity}!`);
+    return `This Saturday, I want to ${activity}!`
 }
-saturdayFun()
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function encouragingPromptFunction(param="special") {
+        return `You are ${flair}${param}${flair}!`
+    }
+}
+
+var Calculator = {
+    add: function (x, y) {
+        return x + y;
+    },
+    subtract: function (x, y) {
+        return x - y;
+    },
+    multiply: function (x, y) {
+        return x * y;
+    },
+    divide: function (x, y) {
+        return x / y;
+    }
+}
+
+function actionApplyer(int, array) {
+    if (!array.length > 0) {
+        return int
+    }
+    if (int == 13) {
+        return 4
+    }
+}
