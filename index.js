057d15ed038fcef54b9c829f72fb256e6faf3dc9
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-06-15 22:17:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work="go to the office") {
+  return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(flair="*") {
+  return function(adjective="special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  }
+}
+
+let Calculator = {
+  add: function(num1, num2) {return num1 + num2},
+  subtract: function(num1, num2) {return num1 - num2},
+  multiply: function(num1, num2) {return num1 * num2},
+  divide: function(num1, num2) {return num1 / num2}
+};
+
+function actionApplyer(number, functions) {
+  for (let i = 0; i < functions.length; i++) {
+    number = functions[i](number);
+  }
+  return number;
+}
