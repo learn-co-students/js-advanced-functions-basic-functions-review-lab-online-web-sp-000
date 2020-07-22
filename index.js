ea799d8f8fb87f8a3d47b3989bcdc59ab8535d74
SPerfors <stevecanbuild@gmail.com>
2020-07-22 17:25:05 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+function saturdayFun(activity='roller-skate') {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(job='go to the office') {
+    return (`This Monday, I will ${job}.`)
+}
+
+function wrapAdjective(flair='*') {
+    return function(adjective='special') {
+        return (`You are ${flair}${adjective}${flair}!`)
+    }
+}
