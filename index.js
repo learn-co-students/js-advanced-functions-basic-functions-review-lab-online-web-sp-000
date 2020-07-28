47f4281135c4aea1927c476194decced40975184
gabejoseph <gabriel.joseph4@gmail.com>
2020-07-28 13:50:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function saturdayFun (saturday = "roller-skate") {
+    return `This Saturday, I want to ${saturday}!`
+}
+
+function mondayWork (monday = "go to the office") {
+    return `This Monday, I will ${monday}.`
+}
+
+function wrapAdjective (variable1 = "*") {
+    return function (variable2 = "special") {
+        return `You are ${variable1}${variable2}${variable1}!`
+    } 
+}
+
+let Calculator = {
+    add (a, b) {
+        return a + b
+    },
+    subtract (a, b) {
+        return a - b
+    },
+    multiply (a, b) {
+        return a * b
+    },
+    divide (a, b) {
+        return a / b
+    }
+}
+
+function actionApplyer (start, array) {
+    // if array === []
+    //     return start
+}
+
