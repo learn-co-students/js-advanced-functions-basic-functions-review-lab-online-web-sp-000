9a9412706e3af7d39698b408f7bec7b9cb281e4e
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-08-05 15:36:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity='go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(a='*') {
+    return function(b='special') {
+       return `You are ${a}${b}${a}!` 
+    }
+}
+
