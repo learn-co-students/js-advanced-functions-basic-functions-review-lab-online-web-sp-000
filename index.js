ccce3c2e8662f43acf9baf3c14c197e6bdd20985
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-06 01:55:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(something="special") {
+        return `You are ${flair}${something}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: function(a, b) {
+        return a + b
+    }, 
+    subtract: function(a, b) {
+        return a - b
+    }, 
+    multiply: function(a, b) {
+        return a * b
+    }, 
+    divide: function(a, b) {
+        return a / b
+    }
+}
+
+function actionApplyer(start, array) {
+        let returnValue = start
+        for (const element of array) {
+            returnValue = element(returnValue)
+        }
+        return returnValue
+
+}
