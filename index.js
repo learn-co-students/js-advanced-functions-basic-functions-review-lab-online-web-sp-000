45077e32a2dc5889d55afd0a6bb152cb94929f8c
github username <github email address>
2020-09-18 19:44:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+
+//function saturdayFun
+//returns a string : This Saturday, I want to roller-skate!
+//activity is passed in as the first parameter 
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+    }
+
+//activity is what is passed in as the first parameter 
+    function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+    }
+    
+    //
+    function wrapAdjective(flair = "*", msg = "You are ") {
+        return function (any = "special") {
+        return `${msg}${flair}${any}${flair}!`
+      }
+      }
+    
+      const Calculator = {
+        add: function (a, b) {return a + b},
+        subtract: function (a, b){return a-b},
+        multiply: function (a, b){return a*b},
+        divide: function (a, b){return a/b}
+      }
+    
+    
+      function actionApplyer (integer, array) {
+        for(let i=0; i < array.length; i++){
+          integer = array[i](integer)
+        }
+        return integer
+      }
