b36cbfba13cb4b37667e11aa1fa7b496bf8a80f3
chienleow <chienleow1@gmail.com>
2020-10-24 17:12:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// function declarations
 function saturdayFun(activity = "roller-skate") {
     return(`This Saturday, I want to ${activity}!`);
 }
@@ -8,9 +9,35 @@ let mondayWork = function(activity = "go to the office") {
 
 function wrapAdjective(value = "*") {
     let innerFunction = function(adjective = "special") {
-        return `You are ${value}${adjective}${value}!`
+        return `You are ${value}${adjective}${value}!`;
     }
-    return innerFunction
+    return innerFunction;
+}
+
+const Calculator = {
+    add: function(a,b) {
+        return a + b;
+    },
+    subtract: function(a,b) {
+        return a - b;
+    },
+    multiply: function(a,b) {
+        return a * b;
+    },
+    divide: function(a,b) {
+        return a / b;
+    }
+}
+
+// function expression
+const actionApplyer = (number, arrayOfFunction) => {
+    if (arrayOfFunction.length === 0) {
+        return number
+    }
+    arrayOfFunction.forEach(func => {
+        number = func(number)
+    });
+    return number
 }
 
 
