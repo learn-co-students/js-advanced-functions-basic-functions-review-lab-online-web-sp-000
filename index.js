a5129a55a1588800bbf4abd7b3fa4953441f047b
mdacht777 <mdacht777@gmail.com>
2020-10-26 12:09:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(action='roller-skate'){
+    return `This Saturday, I want to ${action}!`
+}
+
+function mondayWork(action='go to the office'){
+    return `This Monday, I will ${action}.`
+}
+
+function wrapAdjective(arg="*"){
+    return function (adj="special"){
+        return `You are ${arg}${adj}${arg}!`
+    }
+
+}
+
+const Calculator = {
+    add: function(a,b) {
+        return a+b
+    },
+    subtract: function(a,b) {
+        return a-b
+    },
+    multiply: function(a,b) {
+        return a*b
+    },
+    divide: function(a,b) {
+        return a/b
+    }
+}
+
+function actionApplyer(start,fxs){
+    for (let i=0;i<fxs.length;i++){
+        start = fxs[i](start)
+    }
+    return start
+}
+
