4e4fe7588ab0f04b50745179231c4ed68deaa28c
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-05 18:58:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+};
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+};
+
+function wrapAdjective(flair="*") {
+  return function(adj='special') {
+    return `You are ${flair}${adj}${flair}!`
+  }
+};
+
+let Calculator = {
+  'add': function() {
+    return 1 + 3;
+  },
+  'subtract': function() {
+    return 1 - 3;
+  },
+  'multiply': function() {
+    return 1 * 3;
+  },
+  'divide': function() {
+    return 10 / 5;
+  }
+};
+//
+// function actionApplyer(start, array) {
+//
+// };
