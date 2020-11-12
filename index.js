5b619c3c39073bd5edecd8c22655d5d91dddb19d
ShaylaWhite <shayladwhite1@gmail.com>
2020-11-11 18:06:09 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(arg = "go to the office") {
+    return `This Monday, I will ${arg}.`
+}
+
+let wrapAdjective = function(Shayla="*") {
+    return function(sucess="special") {
+        return `You are ${Shayla}${sucess}${Shayla}!`
+
+    }
+}
+
+const Calculator = {
+    add: function(a,b) {
+      return a + b;
+    },
+    subtract: function(a,b) {
+      return a - b
+    },
+    multiply: function(a,b) {
+      return a * b
+    },
+    divide: function(a,b) {
+      return a / b;
+    }
+  }
+  
+  let actionApplyer = function(start, ray) {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+  
+    return a
+  }
