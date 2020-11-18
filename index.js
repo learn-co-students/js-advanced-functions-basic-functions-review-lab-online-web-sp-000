119f64faf1a460f5154477fc024c236306562689
murphcoder <murphcoder@gmail.com>
2020-11-18 14:46:19 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+function wrapAdjective(symbol = "*", message = "You are ") {
+    return function(word = "special") {
+        return `${message + symbol + word + symbol}!`;
+    };
+};
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+let Calculator = {
+    add: function(num1, num2) {
+        return num1 + num2;
+    },
+    subtract: function(num1, num2) {
+        return num1 - num2;
+    },
+    multiply: function(num1, num2) {
+        return num1 * num2;
+    },
+    divide: function(num1, num2) {
+        return num1 / num2;
+    }
+};
+
+function actionApplyer(number, actions) {
+    for (const action of actions) {
+        number = action(number);
+    };
+    return number;
+};
