0336ced7f393cd360ce54d2ddd4d69985a6d12fa
Yumacorn <ReachRyanMui@gmail.com>
2020-12-28 22:53:15 -0500

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,36 @@ function saturdayFun(activity = "roller-skate") {
 let mondayWork = function(activity = "go to the office") {
     return `This Monday, I will ${activity}.`;
 }
+
+function wrapAdjective(exp = "*") {
+     return function(param = "special") {
+            return `You are ${exp}${param}${exp}!`
+    }
+}
+
+var Calculator = {
+    add: function(a,b) { return a + b; },
+    subtract: function(a,b) { return a - b; },
+    multiply: function(a,b) { return a * b; },
+    divide: function(a,b) { return a / b; }
+
+};
+
+// Calculator.add(a, b) = function(num1, num2) {num1 + num2};
+
+//     function subtract() {
+
+//     }
+// ;
+
+function actionApplyer(start, set) {
+    if (set === []) {
+        return start
+    } else {
+        let result = start
+        return function() {
+            result = set[0];
+        }
+        // return result
+    }
+}
