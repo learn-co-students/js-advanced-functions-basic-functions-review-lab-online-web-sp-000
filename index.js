45093a652e4135969a3ad2d5b2e80835d18e1e51
milos381 <milos_popovic@outlook.com>
2021-01-09 13:57:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function wrapAdjective(paramOuter = "*") {
+    return function (paramOne = "special") {
+        return `You are ${paramOuter}${paramOne}${paramOuter}!`
+    }
+}
+let encouragingPromptFunction = wrapAdjective("!!!")
+encouragingPromptFunction();
+  
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+  
+  let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+  const Calculator = {
+    add: function(a,b) {
+      return a + b;
+    },
+    subtract: function(a,b) {
+      return a - b
+    },
+    multiply: function(a,b) {
+      return a * b
+    },
+    divide: function(a,b) {
+      return a / b;
+    }
+  }
+function actionApplyer(intiger, array) {
+    let start = intiger;
+    for (let i = 0; i < array.length; i++ ){
+        start = array[i](start)
+      }
+    return start;
+}
