c5cd03d991c26e5267fd1226c532e420da3043d1
learn-co-git <argus.two.2@protonmail.com>
2021-01-23 20:30:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity='go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+
+function wrapAdjective(string='*', parameter='special') {
+  return function(parameter) {
+    return `You are ${string}${parameter}${string}!`
+  }
+}
+
+let encouragingPromptFunction = wrapAdjective("!!!")
+
+wrapAdjective("%")("a dedicated programmer")
+
+let Calculator = {}
+Calculator.add = (a, b) => a + b
+Calculator.subtract = (a, b) => a - b
+Calculator.multiply = (a, b) => a * b
+Calculator.divide = (a, b) => a / b
+
+
+function actionApplyer(start, functionArray) {
+  let total = start
+  if (functionArray.length !== 0) {
+    functionArray.forEach(op => {
+      total = op(start)
+      start = total
+    })
+  }
+  return total
+}
