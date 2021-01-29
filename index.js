02d82ad1edbd17a8ddce575594a7e35f3fad2b27
ArslanInfinite <ali.arslan319@gmail.com>
2021-01-28 22:12:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+mondayWork('work from home')
+
+function wrapAdjective (highlight = '*') {
+  return function (whatever = 'special') {
+    return `You are ${highlight}${whatever}${highlight}!`
+  }
+}
+
+const Calculator = {
+  add: function (one, three) {
+    return one + three
+  },
+  subtract: function (one, three) {
+    return one - three
+  },
+  multiply: function (one, three) {
+    return one * three
+  },
+  divide: function (ten, five) {
+    return ten / five
+  }
+}
+
+function actionApplyer(int, arr) {
+  let a = int
+
+  for (let i = 0; i < arr.length; i++) {
+    a = arr[i](a)
+  }
+  return a
+}
