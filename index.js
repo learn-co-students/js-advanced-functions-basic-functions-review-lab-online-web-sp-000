dc55683a45803a7d1dc60b9e50bfd86e64338b41
bongofury <jtroutmanman@gmail.com>
2021-05-21 16:53:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(work="go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(flair="*") {
+    return function(foo="special") {
+        return `You are ${flair}${foo}${flair}!`;
+    }
+}
+
+const Calculator = {
+    add: function(num1, num2) {
+        return num1 + num2;
+    },
+    subtract: function(num1, num2) {
+        return num1 - num2;
+    },
+    multiply: function(num1, num2) {
+        return num1 * num2;
+    },
+    divide: function(num1, num2) {
+        return num1 / num2;
+    }
+};
+
+function actionApplyer(baseInt, arrayOfFns=[]) {
+    if (arrayOfFns.length === 0) {
+        return baseInt;
+    } else {
+        arrayOfFns.forEach(fn => {
+            baseInt = fn(baseInt);
+        });
+        return baseInt;
+    }
+}
