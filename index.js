dc21abb927ea2131d7d365df8e65496a36d27d88
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-25 18:32:58 -0600

    Done.

--- index.js
+++ index.js
@@ -7,9 +7,38 @@ function mondayWork(thing = "go to the office") {
     return `This Monday, I will ${thing}.`
 }
 
-function wrapAdjective(flair) {
-    function innerFucntion(adjective = "special") {
-        return `You are ${flair} ${adjective} ${flair}!`
+function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`
     }
 
+}
+
+const Calculator = {
+    add:
+        function (x, y) {
+            return x + y
+        }
+    ,
+    subtract:
+        function (x, y) {
+            return x - y
+        },
+    multiply:
+        function (x, y) {
+            return x * y
+        },
+    divide:
+        function (x, y) {
+            return x / y
+        }
+}
+
+function actionApplyer(start, args) {
+    if (args.length > 0) {
+        args.forEach(fn => {
+            start = fn(start)
+        })
+    }
+    return start
 }
