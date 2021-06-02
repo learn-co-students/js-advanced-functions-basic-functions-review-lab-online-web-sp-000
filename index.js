239d7af75a5e19579f71849884ec77ee866b4e26
Luis Martinez <luis_mmartinez@live.com>
2021-06-01 22:51:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+};
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+};
+
+function wrapAdjective(flair = "*" ) {
+    return function blabla(word = "special") {
+        return `You are ${flair}${word}${flair}!`;
+    }
+}
+
+const Calculator = { 
+  add:function add() {
+    return (1 + 3);
+  },   
+  subtract: function subtract() {
+    return 1 - 3;
+  },
+  multiply: function multiply() {
+    return 1 * 3;
+  },
+  divide: function divide() {
+    return 10/5;
+  }
+} 
+
+
+function actionApplyer(integer, array) {
+  array.forEach(fun => integer = fun(integer))
+  return integer 
+}; 
